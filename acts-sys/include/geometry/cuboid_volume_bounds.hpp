d653b9fd823a05c8d4c2ae6c3f508e95b4d76485
Daniel Duque <daniel.duque@cern.ch>
2025-08-17 00:03:15 -0700

    feat: Add CuboidVolumeBoundsFace bindings

---
+++ acts-sys/include/geometry/cuboid_volume_bounds.hpp
@@ -0,0 +1,11 @@
+#pragma once
+
+#include "Acts/Geometry/CuboidVolumeBounds.hpp"
+
+namespace acts_sys {
+namespace ffi {
+
+using CuboidVolumeBoundsFace = Acts::CuboidVolumeBounds::Face;
+
+} // namespace ffi
+} // namespace acts_sys
