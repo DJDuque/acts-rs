dac2b71ec5bafb6ba41aa7dc382e87d23ec9412b
Daniel Duque <daniel.duque@cern.ch>
2025-08-17 23:04:57 -0700

    feat: Add PlanarBounds bindings

---
+++ acts-sys/src/surfaces/planar_bounds.rs
@@ -0,0 +1,12 @@
+pub use ffi::*;
+
+#[cxx::bridge(namespace = "Acts")]
+mod ffi {
+    unsafe extern "C++" {
+        include!("Acts/Surfaces/PlanarBounds.hpp");
+
+        type PlanarBounds;
+    }
+
+    impl UniquePtr<PlanarBounds> {}
+}
