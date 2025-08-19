2ab6f7cfbfab30fdd2f79830b259e96566aaa345
Daniel Duque <daniel.duque@cern.ch>
2025-08-18 23:30:09 -0700

    feat: Add LayerBlueprintNode::setSurfaces bindings

--- acts-sys/src/geometry/layer_blueprint_node.rs
+++ acts-sys/src/geometry/layer_blueprint_node.rs
@@ -1,9 +1,25 @@
+use crate::surfaces::surface::Surface;
+use cxx::{ExternType, SharedPtr};
+
 pub use ffi::*;
+
+// See https://github.com/dtolnay/cxx/issues/774 for why this is necessary.
+// If the situation improves upstream, change this.
+#[repr(transparent)]
+pub struct SharedSurface {
+    pub ptr: SharedPtr<Surface>,
+}
+
+unsafe impl ExternType for SharedSurface {
+    type Id = cxx::type_id!("acts_sys::ffi::SharedSurface");
+    type Kind = cxx::kind::Trivial;
+}
 
 #[cxx::bridge(namespace = "Acts::Experimental")]
 mod ffi {
     unsafe extern "C++" {
         include!("Acts/Geometry/LayerBlueprintNode.hpp");
+        include!("acts-sys/include/geometry/layer_blueprint_node.hpp");
         include!("acts-sys/include/helpers.hpp");
 
         type LayerBlueprintNode;
@@ -18,5 +34,18 @@ mod ffi {
         fn upcast_unique_layer_blueprint_node(
             node: UniquePtr<LayerBlueprintNode>,
         ) -> UniquePtr<StaticBlueprintNode>;
+
+        #[namespace = "acts_sys::ffi"]
+        fn layer_blueprint_node_set_surfaces(
+            node: Pin<&mut LayerBlueprintNode>,
+            surfaces: &CxxVector<SharedSurface>,
+        );
+    }
+
+    extern "C++" {
+        include!("acts-sys/include/geometry/layer_blueprint_node.hpp");
+
+        #[namespace = "acts_sys::ffi"]
+        type SharedSurface = crate::geometry::layer_blueprint_node::SharedSurface;
     }
 }
