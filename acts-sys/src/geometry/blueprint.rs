3338faaf40b41bf9455451d685bf0d599b006255
Daniel Duque <daniel.duque@cern.ch>
2025-08-19 18:18:14 -0700

    feat: Add Blueprint constructor bindings

--- acts-sys/src/geometry/blueprint.rs
+++ acts-sys/src/geometry/blueprint.rs
@@ -5,6 +5,9 @@ mod ffi {
     unsafe extern "C++" {
         include!("Acts/Geometry/Blueprint.hpp");
         include!("acts-sys/include/geometry/blueprint.hpp");
+        include!("acts-sys/include/helpers.hpp");
+
+        type Blueprint;
 
         #[namespace = "acts_sys::ffi"]
         type BlueprintConfig;
@@ -13,5 +16,9 @@ mod ffi {
 
         #[namespace = "acts_sys::ffi"]
         fn new_blueprint_config(envelope: &ExtentEnvelope) -> UniquePtr<BlueprintConfig>;
+
+        #[namespace = "acts_sys::ffi"]
+        #[cxx_name = "make_unique"]
+        fn new_blueprint(config: &BlueprintConfig) -> UniquePtr<Blueprint>;
     }
 }
