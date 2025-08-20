19005e6bb31568997e5b8063fa62c5e206b73640
Daniel Duque <daniel.duque@cern.ch>
2025-08-20 12:28:22 -0700

    chore: Remove unnecessary header

--- acts-sys/src/geometry/container_blueprint_node.rs
+++ acts-sys/src/geometry/container_blueprint_node.rs
@@ -4,9 +4,6 @@ pub use ffi::*;
 mod ffi {
     unsafe extern "C++" {
         include!("Acts/Geometry/ContainerBlueprintNode.hpp");
-        // See https://github.com/acts-project/acts/pull/4556
-        // Remove this include if/when the PR is merged
-        include!("Acts/Geometry/PortalShell.hpp");
         include!("acts-sys/include/helpers.hpp");
 
         type CylinderContainerBlueprintNode;
