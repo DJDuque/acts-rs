be27138ac3058992d8753e5c729d0e6974fd79b1
Daniel Duque <daniel.duque@cern.ch>
2025-08-19 19:14:54 -0700

    feat: Add Blueprint::construct bindings

--- acts-sys/src/geometry/blueprint.rs
+++ acts-sys/src/geometry/blueprint.rs
@@ -8,11 +8,19 @@ mod ffi {
         include!("acts-sys/include/helpers.hpp");
 
         type Blueprint;
+        type BlueprintOptions = crate::geometry::blueprint_options::BlueprintOptions;
 
         #[namespace = "acts_sys::ffi"]
         type BlueprintConfig;
+
         #[namespace = "Acts"]
         type ExtentEnvelope = crate::geometry::extent::ExtentEnvelope;
+        #[namespace = "Acts"]
+        type GeometryContext = crate::geometry::geometry_context::GeometryContext;
+        #[namespace = "Acts"]
+        type Logger = crate::utilities::logger::Logger;
+        #[namespace = "Acts"]
+        type TrackingGeometry = crate::geometry::tracking_geometry::TrackingGeometry;
 
         #[namespace = "acts_sys::ffi"]
         fn new_blueprint_config(envelope: &ExtentEnvelope) -> UniquePtr<BlueprintConfig>;
@@ -20,5 +28,12 @@ mod ffi {
         #[namespace = "acts_sys::ffi"]
         #[cxx_name = "make_unique"]
         fn new_blueprint(config: &BlueprintConfig) -> UniquePtr<Blueprint>;
+
+        fn construct(
+            self: Pin<&mut Blueprint>,
+            options: &BlueprintOptions,
+            context: &GeometryContext,
+            logger: &Logger,
+        ) -> UniquePtr<TrackingGeometry>;
     }
 }
