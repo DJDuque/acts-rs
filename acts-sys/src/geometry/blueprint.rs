26372ea51c9c42f820a7adba65f264e3cac04045
Daniel Duque <daniel.duque@cern.ch>
2025-08-19 20:42:46 -0700

    feat: Add Blueprint::addChild bindings

--- acts-sys/src/geometry/blueprint.rs
+++ acts-sys/src/geometry/blueprint.rs
@@ -8,6 +8,7 @@ mod ffi {
         include!("acts-sys/include/helpers.hpp");
 
         type Blueprint;
+        type BlueprintNode = crate::geometry::blueprint_node::BlueprintNode;
         type BlueprintOptions = crate::geometry::blueprint_options::BlueprintOptions;
 
         #[namespace = "acts_sys::ffi"]
@@ -21,6 +22,16 @@ mod ffi {
         type Logger = crate::utilities::logger::Logger;
         #[namespace = "Acts"]
         type TrackingGeometry = crate::geometry::tracking_geometry::TrackingGeometry;
+
+        // This is a "special" node (the root), the gymnastics of upcasting and
+        // then downcasting would be weird to handle in Rust (opposed to the
+        // other nodes that only need to upcast).
+        // Hence this special shim for the root node.
+        #[namespace = "acts_sys::ffi"]
+        fn blueprint_add_child(
+            node: Pin<&mut Blueprint>,
+            child: SharedPtr<BlueprintNode>,
+        ) -> Result<Pin<&mut Blueprint>>;
 
         #[namespace = "acts_sys::ffi"]
         fn new_blueprint_config(envelope: &ExtentEnvelope) -> UniquePtr<BlueprintConfig>;
