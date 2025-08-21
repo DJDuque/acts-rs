07fb4af5ed26e4a8871cb6be7c5af4ebe4461cf2
Daniel Duque <daniel.duque@cern.ch>
2025-08-20 21:55:29 -0700

    feat: Add IVisualization3D::write bindings

--- acts-sys/src/visualization/i_visualization3d.rs
+++ acts-sys/src/visualization/i_visualization3d.rs
@@ -4,8 +4,12 @@ pub use ffi::*;
 mod ffi {
     unsafe extern "C++" {
         include!("Acts/Visualization/IVisualization3D.hpp");
+        include!("acts-sys/include/visualization/i_visualization3d.hpp");
 
         type IVisualization3D;
+
+        #[namespace = "acts_sys::ffi"]
+        fn i_visualization3d_write(vis: &IVisualization3D, path: &CxxString);
     }
 
     impl UniquePtr<IVisualization3D> {}
